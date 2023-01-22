d85c2feafa67f58bce856a91e3229c6925116fcc
Andrey Uvarov <uvarov6799@gmail.com>
2023-01-23 01:46:31 +0300

    Настройка темной темы

--- js/script.js
+++ js/script.js
@@ -1,9 +1,54 @@
-function testArray(a, b) {
-	var sum = 0
-	a.forEach(element => sum += element);
-	b.forEach(element => sum += element);
-	return sum
-}
-testArray([0, 1, 5, 3], [3])
-//---------------------------
+let isChecked = false;
 
+function onBtnUp() {
+	const tl = gsap.timeline();
+
+	isChecked = !isChecked;
+
+	let x = 0;
+	let backgroundColor = "#827D96";
+	let size = "20px";
+
+	if (isChecked) {
+		x = 28;
+		backgroundColor = "#FFFFFF";
+		size = "80px";
+		document.body.classList.toggle('dark')
+		localStorage.setItem('darkMode', 'dark')
+	} else {
+		document.body.classList.toggle('dark')
+		localStorage.setItem('darkMode', 'light')
+	}
+
+	tl.to(".knob", { x, duration: 0.7 }, "up");
+	tl.to(".top", { backgroundColor, duration: 0.7 }, "up");
+	tl.to(".light", { width: size, height: size, duration: 1 }, "up");
+}
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("mouseup", onBtnUp);
+
+// const btnDarkMode = document.querySelector(".btn");
+
+// // Проверка темной темы в localStorage
+
+if (localStorage.getItem('darkMode') == 'dark') {
+	document.body.classList.add("dark");
+}
+
+// // Проверка системных настроек
+// var darkSystem = (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark").matches);
+// if (darkSystem) {
+// 	document.body.classList.add("dark");
+// }
+
+// // Включение ночного режима по кнопке
+// btnDarkMode.onclick = () => {
+// 	const isDark = document.body.classList.toggle('dark');
+
+// 	if (isDark) {
+// 		localStorage.setItem('darkMode', 'dark')
+// 	} else {
+// 		localStorage.setItem('darkMode', 'light')
+// 	}
+// }
